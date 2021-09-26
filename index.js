22184ab77a1ddb0f86ad6e14d19478735380f5aa
JD <jdmejia712@gmail.com>
2021-09-26 09:12:57 -0400

    commit tree da0d58e31758f1ac5f4b2a7a201fd2237bcf88fc

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function saturdayFun(toDo = 'roller-skate'){
+    return `This Saturday, I want to ${toDo}!`
+  }
+
+
+//   function mondayWork(iWillDo = 'go to the office'){
+//     return `This Monday, I will ${iWillDo}.`
+//   }
+
+const mondayWork = function(iWillDo = 'go to the office'){return `This Monday, I will ${iWillDo}.`}
+
