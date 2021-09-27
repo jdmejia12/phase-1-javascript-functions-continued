1754a0ef762e6ce65ac400e1dd541f8b73e9f894
JD <jdmejia712@gmail.com>
2021-09-27 00:01:47 -0400

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(toDo = 'roller-skate'){
+    return `This Saturday, I want to ${toDo}!`
+  }
+
+const mondayWork = function(iWillDo = 'go to the office'){return `This Monday, I will ${iWillDo}.`}
+
+function wrapAdjective(popPop = '*'){
+    const innerWrap = function (uAre = "special"){
+      return `You are ${popPop}${uAre}${popPop}!`;
+  }
+  return innerWrap
+  }
+  
